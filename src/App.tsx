5fdf6c21122319c1b125c46e15e276014a0ef2b4
YunLong Zhou <yunlong.zhou@thoughtworks.com>
2021-07-15 12:11:12 +0800

    [chore] add eslint & prettier

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,8 @@ import React, { useState } from 'react'
 import logo from './logo.svg'
 import './App.css'
 
-function App() {
+function App(): React.ReactElement {
   const [count, setCount] = useState(0)
-
   return (
     <div className="App">
       <header className="App-header">
